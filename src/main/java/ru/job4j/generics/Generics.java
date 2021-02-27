47b26c3e2493b4dd722c6cab57a6a211d0dff477
ZuewOleg <zuev_17@bk.ru>
2021-02-27 11:06:15 +0500

    0. Что такое обобщенные типы (generics) [#479320]

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -19,14 +19,14 @@ public class Generics {
         gen.printObject(third);
         System.out.println();
 
-        gen.printBoundedWildCard(first);
+        gen.printBoundedWildCard(first); /* "Ограничение сверху" должно быть до типа Animal */
         gen.printBoundedWildCard(second);
         gen.printBoundedWildCard(third);
         System.out.println();
 
         gen.printLowerBoundedWildCard(first);
         gen.printLowerBoundedWildCard(second);
-        gen.printLowerBoundedWildCard(third);
+        gen.printLowerBoundedWildCard(third); /* "Ограничение снизу" должно быть до типа Tiger */
     }
 
     public void printObject(List<?> list) {
@@ -36,14 +36,14 @@ public class Generics {
         }
     }
 
-    public void printBoundedWildCard(List<? extends Animal> list) {
-        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
     }
 
-    public void printLowerBoundedWildCard(List<? super Tiger> list) {
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
         for (Iterator<? super Tiger> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
