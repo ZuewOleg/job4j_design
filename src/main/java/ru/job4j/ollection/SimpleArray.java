1df91be02aca565c91fe05e6597a768739fb3097
ZuewOleg <zuev_17@bk.ru>
2021-03-01 14:44:19 +0500

    1. Динамический список на массиве. [#479297]

--- src/main/java/ru/job4j/ollection/SimpleArray.java
+++ src/main/java/ru/job4j/ollection/SimpleArray.java
@@ -1,13 +1,15 @@
 package ru.job4j.ollection;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SimpleArray<T> implements Iterable<T> {
-    private final T[] array;
+    transient T[] array;
     private int size;
     private int index;
+    int modCount = 0;
 
     public SimpleArray(int size) {
         this.array = (T[]) new Object[size];
@@ -18,24 +20,31 @@ public class SimpleArray<T> implements Iterable<T> {
         return this.array[index];
     }
 
-    public void add(T model, T[] array, int s) {
-        if (s == array.length) {
-            array = Arrays.copyOf(array, size + s);
-            array[s] = model;
-            size = s + 1;
+    public void add(T model) {
+        if (this.array[index] == null) {
+            this.array[index] = model;
+        } else if (index == array.length) {
+            Arrays.copyOf(array, size + index);
+            array[index] = model;
+            size = index + 1;
         }
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            int expectedModCount = modCount;
+
             @Override
             public boolean hasNext() {
                 return index < size;
             }
 
             @Override
-            public T next() {
+            public T next() throws ConcurrentModificationException, NoSuchElementException {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
