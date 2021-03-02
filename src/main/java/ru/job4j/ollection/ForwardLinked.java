6df0cec5d90a828019a5668c65f1fc343b742613
ZuewOleg <zuev_17@bk.ru>
2021-03-02 21:58:47 +0500

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#479295]

--- src/main/java/ru/job4j/ollection/ForwardLinked.java
+++ src/main/java/ru/job4j/ollection/ForwardLinked.java
@@ -5,9 +5,11 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private Node<T> last;
+
 
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
+        Node<T> node = new Node<>(value, null, null);
         if (head == null) {
             head = node;
             return;
@@ -19,6 +21,19 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    public void addLast(T value) {
+        Node<T> nodeLast = new Node<T>(value, null, last);
+        if (last == null) {
+            last = nodeLast;
+            return;
+        }
+        Node<T> tail = last;
+        while (tail.prev != null) {
+            tail = tail.prev;
+        }
+        tail.prev = nodeLast;
+    }
+
     public T deleteFirst() {
         if (head == null) {
             throw new NoSuchElementException();
@@ -28,9 +43,18 @@ public class ForwardLinked<T> implements Iterable<T> {
         return value;
     }
 
+    public T deleteLast() {
+        if (last == null) {
+            throw new NoSuchElementException();
+        }
+        T value = last.value;
+        last = last.prev;
+        return value;
+    }
+
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
+        return new Iterator<>() {
             Node<T> node = head;
 
             @Override
@@ -53,10 +77,12 @@ public class ForwardLinked<T> implements Iterable<T> {
     private static class Node<T> {
         T value;
         Node<T> next;
+        Node<T> prev;
 
-        public Node(T value, Node<T> next) {
+        public Node(T value, Node<T> next, Node<T> last) {
             this.value = value;
             this.next = next;
+            this.prev = last;
         }
     }
 }
---
+++ src/main/java/ru/job4j/ollection/SimpleStack.java
@@ -0,0 +1,13 @@
+package ru.job4j.ollection;
+
+public class SimpleStack<T> {
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return linked.deleteLast();
+    }
+
+    public void push(T value) {
+        linked.addLast(value);
+    }
+}
