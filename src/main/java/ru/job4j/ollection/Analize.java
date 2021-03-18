ba5debce54a9143ecbede9fd3fa16d277ac95d86
ZuewOleg <zuev_17@bk.ru>
2021-03-18 21:04:35 +0500

    2. Статистику по коллекции. [#479290]

--- src/main/java/ru/job4j/ollection/Analize.java
+++ src/main/java/ru/job4j/ollection/Analize.java
@@ -4,24 +4,31 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Analize {
     public Info diff(List<User> previous, List<User> current) {
         Info rsl = new Info(0, 0, 0);
-        Map<Integer, String> currents = current.stream().collect(Collectors.toMap(User::getId, User::getName));
-        for (var p : previous) {
-            if (currents.containsKey(p.getId())) {
-                if (!currents.containsValue(p.getName())) {
+        User userMap = new User (0, " ");
+        User userList = new User (0, " ");
+        for (int i = 0; i < current.size(); i++) {
+            Map<Integer, User> currents = current.stream().collect(Collectors.toMap(User::getId, Function.identity()));
+            for (var p : previous) {
+                if (currents.containsKey(p.getId())) {
+                    userMap = currents.get(p.getId());
+                    userList = p;
+                }
+            }
+                if (!userMap.getName().equals(userList.getName())) {
                     rsl.changed++;
                 }
+            if (previous.size() > currents.size()) {
+                rsl.deleted = previous.size() - currents.size();
+            } else if (previous.size() < currents.size()) {
+                rsl.added = currents.size() - previous.size();
             }
-                if (previous.size() > currents.size()) {
-                    rsl.deleted = previous.size() - currents.size();
-                } else if (previous.size() < currents.size()) {
-                    rsl.added = currents.size() - previous.size();
-                }
-            }
+        }
         return rsl;
     }
 
