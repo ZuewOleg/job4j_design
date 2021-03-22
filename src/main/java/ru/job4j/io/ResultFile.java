77264d18b392b73c3e40a49e8610c6ccd9fe17b2
ZuewOleg <zuev_17@bk.ru>
2021-03-22 21:52:20 +0500

    0.4. BufferedOutputStream [#479347]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,10 +1,29 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("Multiple.txt")) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("Multiple.txt")
+                ))) {
+            out.write(("1 * 1 = 1" + System.lineSeparator()));
+            out.write(("1 * 2 = 2" + System.lineSeparator()));
+            out.write(("1 * 3 = 3" + System.lineSeparator()));
+            out.write(("1 * 4 = 4" + System.lineSeparator()));
+            out.write(("1 * 5 = 5" + System.lineSeparator()));
+            out.write(("1 * 6 = 6" + System.lineSeparator()));
+            out.write(("1 * 7 = 7" + System.lineSeparator()));
+            out.write(("1 * 8 = 8" + System.lineSeparator()));
+            out.write(("1 * 9 = 9" + System.lineSeparator()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write(("1 * 1 = 1" + System.lineSeparator()).getBytes());
             out.write(("1 * 2 = 2" + System.lineSeparator()).getBytes());
             out.write(("1 * 3 = 3" + System.lineSeparator()).getBytes());
@@ -16,6 +35,6 @@ public class ResultFile {
             out.write(("1 * 9 = 9" + System.lineSeparator()).getBytes());
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        } */
     }
 }
