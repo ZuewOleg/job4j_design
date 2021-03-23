55c68fd7d1a4ea0eea65391efe14a0447d3c8dc5
ZuewOleg <zuev_17@bk.ru>
2021-03-23 11:12:52 +0500

    0.1. FileOutputStream. [#479345]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -10,15 +10,15 @@ public class ResultFile {
                 new BufferedOutputStream(
                         new FileOutputStream("Multiple.txt")
                 ))) {
-            out.write(("1 * 1 = 1" + System.lineSeparator()));
-            out.write(("1 * 2 = 2" + System.lineSeparator()));
-            out.write(("1 * 3 = 3" + System.lineSeparator()));
-            out.write(("1 * 4 = 4" + System.lineSeparator()));
-            out.write(("1 * 5 = 5" + System.lineSeparator()));
-            out.write(("1 * 6 = 6" + System.lineSeparator()));
-            out.write(("1 * 7 = 7" + System.lineSeparator()));
-            out.write(("1 * 8 = 8" + System.lineSeparator()));
-            out.write(("1 * 9 = 9" + System.lineSeparator()));
+            out.println("1 * 1 = 1");
+            out.println("1 * 2 = 2");
+            out.println("1 * 3 = 3");
+            out.println("1 * 4 = 4");
+            out.println("1 * 5 = 5");
+            out.println("1 * 6 = 6");
+            out.println("1 * 7 = 7");
+            out.println("1 * 8 = 8");
+            out.println("1 * 9 = 9");
         } catch (Exception e) {
             e.printStackTrace();
         }
