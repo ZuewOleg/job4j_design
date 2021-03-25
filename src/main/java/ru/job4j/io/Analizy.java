7b71465b3963e309142ec19f0693358c1c44fe86
ZuewOleg <zuev_17@bk.ru>
2021-03-25 18:01:54 +0500

    2. Анализ доступности сервера. [#479352]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -8,21 +8,26 @@ public class Analizy {
     public static void unavailable(String source, String target) {
         List<String> rsl = new ArrayList<>();
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            String serverDrop = null;
             while (read.ready()) {
                 String status = read.readLine();
-                if (status.startsWith("400") || status.startsWith("500")) {
+                if (serverDrop == null && (status.startsWith("400") || status.startsWith("500"))) {
                     String drop = "Конец работы сервера - " + status.split(" ")[1] + System.lineSeparator();
                     rsl.add(drop);
-                } else {
+                    serverDrop = status;
+                } else if (serverDrop != null && (!status.startsWith("400") && !status.startsWith("500"))) {
                     String up = "Начало работы сервера - " + status.split(" ")[1] + System.lineSeparator();
                     rsl.add(up);
-                }
-                PrintWriter writer = new PrintWriter(new FileOutputStream(target));
-                for (var r : rsl) {
-                    writer.write(r);
+                    serverDrop = null;
                 }
             }
-            System.out.println(rsl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (var r : rsl) {
+                writer.write(r);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,6 +35,6 @@ public class Analizy {
 
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
-        analizy.unavailable("./data/server.log", "./data/target.log");
+        analizy.unavailable("./data/server.txt", "./data/analysis_server.txt");
     }
 }
