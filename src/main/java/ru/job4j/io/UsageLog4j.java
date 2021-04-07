58f86421d77fd85ef86ce9d26d7ea7309d14e45a
ZuewOleg <zuev_17@bk.ru>
2021-04-07 19:16:34 +0500

    3. Slf4j - вывод переменных. [#479339]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Oleg Zuev";
+        byte age = 28;
+        char gender = 'M';
+        double height = 176.3;
+        float weight = 70.5f;
+        int day = 17;
+        short month = 9;
+        long year = 1992L;
+        boolean married = true;
+
+        LOG.debug("User info name : {}, age : {}, gender: {}, height : {}, weight : {}, date of birth: {}.{}.{}, married: {}",
+                name, age, gender, height, weight, day, month, year, married);
     }
 }
