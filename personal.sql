1c8130c43b2b72878da960584784e600fb310f1d
ZuewOleg <zuev_17@bk.ru>
2021-04-19 15:36:56 +0500

    0. Что такое база данных. [#479275]

--- personal.sql
+++ personal.sql
@@ -1,1 +1,8 @@
+insert into personal(name, age, birthday, married, gender) values('Ivan', 35, '1/8/1999', true, 'M');
+select * from personal;
+update personal set name = 'Oleg';
+update personal set age = 28;
+update personal set birthday = '17/09/1992';
+select * from personal;
+delete from personal;
 select * from personal;
