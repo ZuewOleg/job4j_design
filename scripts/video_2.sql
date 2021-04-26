c6dcd4b2e0f449f0e912889dd802b55d8a4bf25d
ZuewOleg <zuev_17@bk.ru>
2021-04-26 09:30:16 +0500

    0. Видео [#479283] (outer join)

--- scripts/video_2.sql
+++ scripts/video_2.sql
@@ -21,20 +21,21 @@ insert into emploees(name, departaments_id) values ('Романов', 2);
 insert into emploees(name, departaments_id) values ('Ленин', 3);
 insert into emploees(name, departaments_id) values ('Сталин', 3);
 
-//Выполнить запросы с left, rigth, full, cross соединениями
+--Выполнить запросы с left, rigth, full, cross соединениями
 select * from emploees e left join departaments d on e.departament_id = d.id;
 select * from emploees e right join departaments d on e.departament_id = d.id;
 select * from departaments d full join emploees e on d.id = e.departament_id;
 
-//Используя left join найти департаменты, у которых нет работников
-select d.name from departaments d left join emploees e d.id != e.departaments_id;
+--Используя left join найти департаменты, у которых нет работников
+select d.name from departaments d left join emploees e on d.id = e.departaments_id
+where e.departaments_id = null;
 
-//Используя left и right join написать запросы, которые давали бы одинаковый результат
+--Используя left и right join написать запросы, которые давали бы одинаковый результат
 select * from departaments d right join emploees e on d.id = e.departament_id;
 select * from emploees e left join departaments d on e.departament_id = d.id;
 
-//Создать таблицу teens с атрибутами name, gender и заполнить ее.
-//Используя cross join составить все возможные разнополые пары
+--Создать таблицу teens с атрибутами name, gender и заполнить ее.
+--Используя cross join составить все возможные разнополые пары
 create table teens(
 	id serial primary key,
 	name varchar(250),
