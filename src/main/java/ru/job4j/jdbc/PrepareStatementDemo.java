42dc3e10c4b74208af1fc6fcf7fb1bd9898a5e41
ZuewOleg <zuev_17@bk.ru>
2021-04-26 12:57:48 +0500

    0.2. PrepareStatement [#479251]

---
+++ src/main/java/ru/job4j/jdbc/PrepareStatementDemo.java
@@ -0,0 +1,106 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrepareStatementDemo {
+
+    private Connection connection;
+
+    public PrepareStatementDemo() throws Exception {
+        initConnection();
+    }
+
+    public void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void insert(City city) { // метод вставки
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean update(City city) { // метод обновления
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("update cities set name = ?, population = ? where id = ?")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.setInt(3, city.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean delete(int id) { // метод удаления
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("delete from cities where id = ?")) {
+            statement.setInt(1, id);
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<City> findAll() { // метод получения всех элементов
+        List<City> cities = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("select * from cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    cities.add(new City(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("population")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+
+    public City insertId(City city) { // метод получения id вставленного элемента
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    city.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return city;
+    }
+
+    public static void main(String[] args) throws Exception {
+        City city1 = new City(1, "Moscow", 12655050);
+        City city2 = new City(2, "Roma", 2870500);
+        City city3 = new City(3, "New York", 8622698);
+        PrepareStatementDemo psd = new PrepareStatementDemo();
+        psd.insert(city1);
+        psd.insert(city2);
+        psd.insert(city3);
+        System.out.println(psd.findAll());
+    }
+}
